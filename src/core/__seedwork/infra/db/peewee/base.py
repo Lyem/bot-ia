c0cbd468e23ba979c151b3735ea201d79c158af7
Fabio Melo <fabiobiotec17@gmail.com>
2023-05-07 17:11:49 -0300

    charge sqlite to mysql

--- src/core/__seedwork/infra/db/peewee/base.py
+++ src/core/__seedwork/infra/db/peewee/base.py
@@ -1,6 +1,11 @@
-from peewee import SqliteDatabase, Model
+import os
+from dotenv import load_dotenv
+from peewee import Model, MySQLDatabase
+load_dotenv()
 
-db = SqliteDatabase('notas.db')
+db = MySQLDatabase(os.getenv('MYSQL_DATABASE'), user=os.getenv('MYSQL_USER'), password=os.getenv(
+    'MYSQL_PASSWORD'), host=os.getenv('MYSQL_HOST'), port=int(os.getenv('MYSQL_PORT')))
+
 
 class BaseModel(Model):
 
